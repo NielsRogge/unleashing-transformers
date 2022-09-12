4d53caa41e95769a8fe95c8d478f7223301997d1
Niels Rogge <nielsrogge@Nielss-MacBook-Pro.local>
2022-09-12 09:34:29 +0200

    Add VQGAN inference script

---
+++ inference_vqgan.py
@@ -0,0 +1,34 @@
+
+from models import VQAutoEncoder
+from hparams import get_sampler_hparams
+from train_sampler import get_sampler
+from utils.sampler_utils import retrieve_autoencoder_components_state_dicts, get_samples
+from utils.log_utils import load_model
+
+from PIL import Image
+import requests
+
+from torchvision.transforms import Compose, Resize, ToTensor, Normalize
+
+
+image_transformations = Compose([Resize, ToTensor, Normalize])
+
+
+def main(H):
+    # step 1: load the VQ-VAE
+    model = VQAutoEncoder(H)
+    model.load_state_dict(H.ae_load_dir, strict=False)
+    
+    # step 2: run forward pass on cats image
+    url = 'http://images.cocodataset.org/val2017/000000039769.jpg'
+    image = Image.open(requests.get(url, stream=True).raw)
+    pixel_values = image_transformations(image).unsqueeze(0)
+    out = model.encoder(pixel_values)
+    print("Shape of out:", out.shape)
+
+    return out
+
+
+if __name__ == "__main__":
+    H = get_sampler_hparams()
+    main(H)
