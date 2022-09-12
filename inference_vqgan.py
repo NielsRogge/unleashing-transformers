97c91f195d7fb0a0528accf1662bcc9b501fadc3
Niels Rogge <nielsrogge@Nielss-MacBook-Pro.local>
2022-09-12 09:49:12 +0200

    Fix another bug

--- inference_vqgan.py
+++ inference_vqgan.py
@@ -16,7 +16,6 @@ from torchvision.transforms import Compose, Resize, ToTensor, Normalize
 image_transformations = Compose([Resize((256,256)),
                                  ToTensor(),
                                  Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])])
-)
 
 
 def main(H):
